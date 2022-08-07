ab09254892dd4e66de1f2026c747c435681b4e96
keymerblack <keimeralexanderj@gmail.com>
2022-08-07 09:13:25 -0500

    se agrega el metodo getDataFromService() para el consumo del service de los personajes

--- src/app/components/character-list/character-list.component.ts
+++ src/app/components/character-list/character-list.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from 'src/app/shared/interfaces/character-interfaces';
+import { CharacterServiceService } from 'src/app/shared/services/character-service.service';
+import { filter, take } from 'rxjs/operators';
+
+type RequestInfo = {
+  next: string | null;
+}
 
 @Component({
   selector: 'app-character-list',
@@ -6,10 +13,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./character-list.component.scss']
 })
 export class CharacterListComponent implements OnInit {
+  public characters: Character[] = [];
+  info: RequestInfo = {
+    next: null,
 
-  constructor() { }
+  }
+  private pageNum = 1;
+  private query!: string;
+  private hideScrollHeight = 200;
+  private showScrollHeight = 500;
+
+  constructor(
+    private characterService: CharacterServiceService
+  ) { }
 
   ngOnInit(): void {
+    this.getDataFromService();
+  }
+
+  private getDataFromService(): void{
+    this.characterService.searchCharacter(this.query, this.pageNum)
+      .pipe(
+        take(1)
+      ).subscribe( (res:any) => {
+        console.log('Response', res)
+        if(res?.results?.length){
+          const { info, results } = res;
+          this.characters = [...this.characters, ...results];
+          this.info = info;
+        }else {
+          this.characters = [];
+        }
+
+      })
   }
 
 }
