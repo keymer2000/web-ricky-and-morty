77d44ce6dad6c948c5713bffa59e103188302ffd
keymerblack <keimeralexanderj@gmail.com>
2022-08-07 13:39:31 -0500

    Se implementa scroll infinito

--- src/app/components/character-list/character-list.component.ts
+++ src/app/components/character-list/character-list.component.ts
@@ -1,92 +1,115 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { Character } from 'src/app/shared/interfaces/character-interfaces';
-import { CharacterService } from 'src/app/shared/services/character.service';
-import { filter, take } from 'rxjs/operators';
-import { ActivatedRoute, NavigationEnd, ParamMap, Router } from '@angular/router';
-import { DOCUMENT } from "@angular/common";
+import { DOCUMENT } from '@angular/common';
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  ParamMap,
+  Router,
+} from '@angular/router';
 
+import { filter, take } from 'rxjs/operators';
+import { Character } from 'src/app/shared/interfaces/character-interfaces';
+
+import { TrackHttpError } from 'src/app/shared/models/trackHttpError';
+import { CharacterService } from 'src/app/shared/services/character.service';
 
 type RequestInfo = {
-  next: string | null;
-}
-
+  next: string;
+};
 @Component({
   selector: 'app-character-list',
   templateUrl: './character-list.component.html',
-  styleUrls: ['./character-list.component.scss']
+  styleUrls: ['./character-list.component.scss'],
 })
 export class CharacterListComponent implements OnInit {
-  public characters: Character[] = [];
+  characters: Character[] = [];
+
   info: RequestInfo = {
     next: null,
+  };
 
-  }
+  showGoUpButton = false;
   private pageNum = 1;
-  private query!: string;
+  private query: string;
   private hideScrollHeight = 200;
   private showScrollHeight = 500;
 
   constructor(
-    @Inject(DOCUMENT) private document:Document,
-    private characterService: CharacterService,
-    private activateRoute:ActivatedRoute,
-    private router:Router
+    @Inject(DOCUMENT) private document: Document,
+    private characterSvc: CharacterService,
+    private route: ActivatedRoute,
+    private router: Router
   ) {
     this.onUrlChanged();
-   }
+  }
 
   ngOnInit(): void {
     this.getCharactersByQuery();
   }
 
-  private onUrlChanged():void {
-    this.router.events.pipe(
-      filter((event) => event instanceof NavigationEnd )
-    ).subscribe(
-      () => {
-        this.characters = [];
-        this.pageNum = 1;
-        this.getCharactersByQuery();
-      }
-    )
-  }
-
-  private getCharactersByQuery():void{
-    this.activateRoute.queryParams.pipe(
-      take(1)).subscribe( (params: ParamMap) => {
-      console.log('params', params)
-      this.query = params['q'];
-      this.getDataFromService();
-    })
-  }
-
-  private getDataFromService(): void{
-    this.characterService.searchCharacter(this.query, this.pageNum)
-      .pipe(
-        take(1)
-      ).subscribe( (res:any) => {
-        console.log('Response', res)
-        if(res?.results?.length){
-          const { info, results } = res;
-          this.characters = [...this.characters, ...results];
-          this.info = info;
-        }else {
-          this.characters = [];
-        }
-
-      })
-  }
-
-  onScrollDown():void{
-    if(this.info.next){
-      this.pageNum++;
-      this.getDataFromService;
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    const yOffSet = window.pageYOffset;
+    if (
+      (yOffSet ||
+        this.document.documentElement.scrollTop ||
+        this.document.body.scrollTop) > this.showScrollHeight
+    ) {
+      this.showGoUpButton = true;
+    } else if (
+      this.showGoUpButton &&
+      (yOffSet ||
+        this.document.documentElement.scrollTop ||
+        this.document.body.scrollTop) < this.hideScrollHeight
+    ) {
+      this.showGoUpButton = false;
     }
   }
 
-  onScrollTop():void{
-    this.document.body.scrollTop = 0; //safari
-    this.document.documentElement.scrollTop = 0; //other browser
+  onScrollDown(): void {
+    if (this.info.next) {
+      this.pageNum++;
+      this.getDataFromService();
+    }
   }
 
+  onScrollTop(): void {
+    this.document.body.scrollTop = 0; // Safari
+    this.document.documentElement.scrollTop = 0; // Other
+  }
+
+  private onUrlChanged(): void {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.characters = [];
+        this.pageNum = 1;
+        this.getCharactersByQuery();
+      });
+  }
+
+  private getCharactersByQuery(): void {
+    this.route.queryParams.pipe(take(1)).subscribe((params: ParamMap) => {
+      this.query = params['q'];
+      this.getDataFromService();
+    });
+  }
+
+  private getDataFromService(): void {
+    this.characterSvc
+      .searchCharacter(this.query, this.pageNum)
+      .pipe(take(1))
+      .subscribe(
+        (res: any) => {
+          if (res?.results?.length) {
+            const { info, results } = res;
+            this.characters = [...this.characters, ...results];
+            this.info = info;
+          } else {
+            this.characters = [];
+          }
+        },
+        (error: TrackHttpError) => console.log(error.friendlyMessage)
+      );
+  }
 }
